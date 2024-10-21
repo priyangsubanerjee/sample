f9ec527937bd1f60f4b02e742ac1788eb6a1fae2
Priyangsu Banerjee <phyrstudio@gmail.com>
2024-10-21 14:32:01 +0530

    Refactor Home component to fetch and display todos from the database

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
+import prisma from "@/prisma/prisma";
 import Image from "next/image";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home() {
+  let todo = await prisma.todo.findMany();
+  console.log(todo);
+  return (
+    <div className="space-y-5">
+      {todo.map((item, i) => (
+        <div key={item.id}>
+          {i} --
+          <h3>{item.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
 }
