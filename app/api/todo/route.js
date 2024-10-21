05ef72ecc2faa195fd9cfc0dafde105d70c2e3ab
Priyangsu Banerjee <phyrstudio@gmail.com>
2024-10-21 14:16:42 +0530

    Refactor todo.js and route.js to add getAllTodos function and prevent caching

--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -1,6 +1,16 @@
 import { getAllTodos } from "@/prisma/todo";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  let todos = await getAllTodos();
-  return Response.json(todos);
+  try {
+    let todos = await getAllTodos();
+    const response = NextResponse.json(todos);
+    response.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    return response;
+  } catch (error) {}
 }
