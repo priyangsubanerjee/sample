7007e7b3d894d1eacaa6c1a268e73389c9fdf1fd
Priyangsu Banerjee <phyrstudio@gmail.com>
2024-10-21 14:21:25 +0530

    Refactor todo.js and route.js to remove caching and update response handling

--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -1,16 +1,8 @@
+export const dynamic = "force-dynamic";
+
 import { getAllTodos } from "@/prisma/todo";
-import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    let todos = await getAllTodos();
-    const response = NextResponse.json(todos);
-    response.headers.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
-    return response;
-  } catch (error) {}
+  let todos = await getAllTodos();
+  return Response.json(todos);
 }
